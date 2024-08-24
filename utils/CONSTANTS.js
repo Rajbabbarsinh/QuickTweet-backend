93d780f7fd8a6c304399ceb374f85b6f2df7ceee
rajbabbar <rajrathva9499@gmail.com>
2024-08-25 03:04:48 +0530

    change in constant

--- utils/CONSTANTS.js
+++ utils/CONSTANTS.js
@@ -1,2 +1,2 @@
-export const CLIENT_BASE_URL = "https://loop-connect.vercel.app" 
-export const SERVER_BASE_URL = "https://loop-backend-2.vercel.app"
+export const CLIENT_BASE_URL = "https://quicktweet-frontend.vercel.app";
+export const SERVER_BASE_URL = "https://quicktweet-backend.vercel.app";
